9658df805725961ba54640517288a6df8f7aa2ac
afawzy.c <afawzy.c@NEC.GOV.SA>
2025-08-10 16:06:46 -0700

    CUSTOMIZE URL

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DBConcction = callback => { mongoose.connect(process.env.MONGO_URL).then( ()=> {
+const DBConcction = callback => { mongoose.connect("mongodb+srv://abdelrahmanawzy98:abdo571998@cluster0.bpdihnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then( ()=> {
          console.log("DB Connected!!")
          callback()
     }).catch(err => {
